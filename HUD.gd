0e76dd086e062070e5330c1aa39d7ffee1d08095
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2021-11-07 11:59:20 -0800

    click spawn

---
+++ HUD.gd
@@ -0,0 +1,15 @@
+extends Control
+
+signal changetype(new_type)
+
+func _on_Button_box_pressed():
+	emit_signal("changetype", "box")
+
+func _on_Button_balloon_pressed():
+	emit_signal("changetype", "balloon")
+
+func _on_Button_rope_pressed():
+	emit_signal("changetype", "rope")
+
+func _on_Button_ramp_pressed():
+	emit_signal("changetype", "ramp")
--- Main.gd
+++ Main.gd
@@ -6,20 +6,14 @@ export (PackedScene) var box
 export (PackedScene) var balloon
 export (PackedScene) var rope
 export (PackedScene) var ramp
-var types
+var current_type = "box"
 
 func _ready():
-	var types = {
-			"box" : box,
-			"balloon" : balloon,
-			"rope": rope,
-			"ramp": ramp
-			}
 	hud_offset = $HUD.get_global_position() -$Player.get_global_position()
 	
 func _process(delta):
 	$Camera2D.set_global_position($Player.get_global_position())
-	$HUD.set_global_position(Vector2($Player.get_global_position() - $HUD.get_size() / 2).floor())
+	$HUD.set_global_position(Vector2($Player.get_global_position() - $HUD.get_size() / 2 + Vector2(0, -30)).floor())
 	counter += delta
 	if counter > 2:
 		counter = 0
@@ -27,11 +21,27 @@ func _process(delta):
 		$HUD/MarginContainer/VBoxContainer/HBoxContainer/Graphite.value -= 5
 		
 func _input(event):
-	if event is InputEventScreenTouch and event.pressed:
-		var target = event.position
-		_spawn_fake_object("box", target)
+	if event is InputEventMouseButton and event.pressed:
+		var target = get_viewport().get_mouse_position() + $Camera2D.position - Vector2(get_viewport().size / 2).floor()
+		print(target)
+		_spawn_fake_object(target)
 		
-func _spawn_fake_object(type, target):
+func _spawn_fake_object(target):
 	var new_obj = box.instance()
+	match current_type:
+		"box":
+			new_obj = box.instance()
+		"balloon":
+			new_obj = balloon.instance()
+		"rope":
+			new_obj = rope.instance()
+		"ramp":
+			new_obj = ramp.instance()	
 	new_obj.position = target
 	add_child(new_obj)
+
+func _change_current_type(new_type):
+	current_type = new_type
+
+func _on_HUD_changetype(new_type):
+	_change_current_type(new_type)
