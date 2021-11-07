001b8a38a28a9fb356549275a132793be1bc988d
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2021-11-07 11:17:03 -0800

    spawn boxeds

--- Main.gd
+++ Main.gd
@@ -2,10 +2,36 @@ extends Node
 
 var counter = 0.0
 var hud_offset = Vector2()
+export (PackedScene) var box
+export (PackedScene) var balloon
+export (PackedScene) var rope
+export (PackedScene) var ramp
+var types
 
 func _ready():
+	var types = {
+			"box" : box,
+			"balloon" : balloon,
+			"rope": rope,
+			"ramp": ramp
+			}
 	hud_offset = $HUD.get_global_position() -$Player.get_global_position()
 	
 func _process(delta):
 	$Camera2D.set_global_position($Player.get_global_position())
-	$HUD.set_global_position($Player.get_global_position() + hud_offset/2)
+	$HUD.set_global_position(Vector2($Player.get_global_position() - $HUD.get_size() / 2).floor())
+	counter += delta
+	if counter > 2:
+		counter = 0
+		$HUD/MarginContainer/VBoxContainer/HBoxContainer/Health.value -= 1
+		$HUD/MarginContainer/VBoxContainer/HBoxContainer/Graphite.value -= 5
+		
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		var target = event.position
+		_spawn_fake_object("box", target)
+		
+func _spawn_fake_object(type, target):
+	var new_obj = box.instance()
+	new_obj.position = target
+	add_child(new_obj)
--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ export (int) var JUMP_SPEED = -500
 export (int) var GRAVITY = 1200
 var velocity = Vector2()
 var jumping = false
+var jump_time = 0.0
 var crouching = false
 var counter = 0
 
@@ -14,18 +15,9 @@ func get_input():
 	var left = Input.is_action_pressed('left')
 	var crouch = Input.is_action_just_pressed('jump')
 	var jump = Input.is_action_just_released('jump')
-
-	if crouching and jump and is_on_floor():
-		jumping = true
-		$AnimatedSprite.play('jump')
-		crouching = false
-		velocity.y += JUMP_SPEED
-		
+	
 	if crouch and is_on_floor():
 		crouching = true
-		$AnimatedSprite.stop()
-		$AnimatedSprite.frame = 0
-		$AnimatedSprite.animation = 'crouch'
 	else:
 		if right:
 			velocity.x += MOVE_SPEED
@@ -34,20 +26,36 @@ func get_input():
 			velocity.x -= MOVE_SPEED
 			$AnimatedSprite.scale.x = -1
 	
+	if crouching and jump and is_on_floor():
+		jumping = true
+		jump_time = 0
+		crouching = false
+		velocity.y += JUMP_SPEED
+		
+
 func _physics_process(delta):
 	get_input()
 	velocity.y += GRAVITY * delta
 	
-	if jumping and is_on_floor():
-		jumping = false
-		$AnimatedSprite.play('idle')
+	_switch_animation()
 	
 	if crouching:
 		velocity.x = 0
-		
-	if not jumping and velocity.x != 0:
+	velocity = move_and_slide(velocity, Vector2.UP, false, 4, 0.785398, false)
+
+	if jumping:
+		jump_time += delta
+		if jump_time > delta * 5:
+			if is_on_floor():
+				jumping = false
+
+	
+func _switch_animation():
+	if crouching:
+		$AnimatedSprite.play('crouch')
+	elif jumping:
+		$AnimatedSprite.play('jump')
+	elif velocity.x == 0:
+		$AnimatedSprite.play('idle')
+	else:
 		$AnimatedSprite.play('walk')
-	
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
-	
